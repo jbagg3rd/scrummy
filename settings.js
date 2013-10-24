11110c22cfa53bcfa265713a2787037d3d8641da
Brian Schmitt <brianschmitt@gmail.com>
2013-10-24 17:49:01 +0000

    Custom room names

--- settings.js
+++ settings.js
@@ -5,21 +5,12 @@ config.port = 8080;
 // These are the valid HTML color names minus Light*, Medium*, and Dark*, to use
 // as room names. The random number generator is still a fallback but these are
 // easier to communicate verbally.
-config.words = ['aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige',
-  'bisque', 'black', 'blanchedalmond', 'blue', 'blueviolet', 'brown', 'burlywood',
-  'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk',
-  'crimson', 'cyan', 'deeppink', 'deepskyblue', 'dimgray', 'dodgerblue', 'firebrick',
-  'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite', 'gold',
-  'goldenrod', 'gray', 'green', 'greenyellow', 'honeydew', 'hotpink', 'indianred',
-  'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon',
-  'lime', 'limegreen', 'linen', 'magenta', 'maroon', 'midnightblue', 'mintcream',
-  'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab',
-  'orange', 'orangered', 'orchid', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum',
-  'powderblue', 'purple', 'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon',
-  'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue',
-  'slategray', 'snow', 'springgreen', 'steelblue', 'tan', 'teal', 'thistle',
-  'tomato', 'turquoise', 'violet', 'wheat', 'white', 'whitesmoke', 'yellow',
-  'yellowgreen'];
+config.words = ['baseload', 'kilowatt', 'peakplant', 'substation', 'transformer', 'transmission',
+  'offpeak', 'deregulation', 'puc', 'ratecase', 'btu', 'naturalgas', 'rates',
+  'commercial', 'industrial', 'residential', 'coal', 'meter', 'voltage',
+  'amperage', 'generation', 'circuit', 'distribution', 'electricity', 'solar', 'windfarm',
+  'lineman', 'hightension', 'breaker', 'threephase', 'renewable', 'utility',
+  'hydroelectric', 'nuclear', 'highvoltage', 'grid', 'tou'];
 config.autoreveal = true;
 config.highlight_highlow = true;
 config.novote_afterreveal = true;
